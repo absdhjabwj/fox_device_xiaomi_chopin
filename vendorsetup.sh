0e7f3356570af0d669f8b0d8dcc4ff910f7ec878
absdhjabwj <92565575+absdhjabwj@users.noreply.github.com>
2025-03-29 13:28:20 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -65,7 +65,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_VERSION="R11.1"
 	export FOX_VARIANT=MIUI
 	export OF_PATCH_AVB20=1
-	export OF_MAINTAINER="Anya1014"
+	export OF_MAINTAINER="xYc4t"
 	export OF_ADVANCED_SECURITY=1
 	export OF_USE_TWRP_SAR_DETECT=1
 	export FOX_VIRTUAL_AB_DEVICE=1
